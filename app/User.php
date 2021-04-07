7bc6f23553df1334340c15d2f695816c555a9eca
poojadbiz <pooja.adbiz@gmail.com>
2021-04-07 23:09:03 +0530

    working on rgistration form

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-         'first_name','last_name' ,'mobile','email', 'password',
+         'first_name','last_name' ,'mobile','email', 'password','gender','city','state','country','designation','years','status'
     ];
 
     /**
