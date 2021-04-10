a25c5cf1474171708736eb969b7099e0dd8dacb3
heenasavaliya2909 <heenasavaliya2704@gmail.com>
2021-04-09 21:40:07 -0700

    register table commit

--- database/migrations/2021_03_19_104106_create_companies_table.php
+++ database/migrations/2021_03_19_104106_create_companies_table.php
@@ -16,6 +16,12 @@ class CreateCompaniesTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
+			$table->string('company_name');
+			$table->string('address');
+			$table->string('second_address');
+			$table->string('latitude');
+			$table->string('longitude');
+			$table->string('number_emp');
 			$table->string('mobile');
             $table->string('email');
             $table->string('password');
