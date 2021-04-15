19e67be217582d312c11a6fa3d6e8289a6daaf70
heenaadbiz <heena.adbiz@gmail.com>
2021-04-15 14:36:10 +0530

    first commit by heena2 and add company table data

--- app/Company.php
+++ app/Company.php
@@ -17,6 +17,11 @@ class Company extends Authenticatable
      */
     protected $fillable = [
          'first_name','last_name' ,'mobile','email', 'password',
+         'company_name',
+         'gst_number','location', 'latitude', 'longitude','city', 'state', 'country', 'pincode',
+         'total_experience', 'years_of_establishment', 'status', 'created_at', 
+         'is_online', 'is_branch_available', 'branch_name', 'branch_place','user_target', 'no_of_employees',
+         'nearby_famous_landmark', 'targeted_age_group',
     ];
 
     /**
--- database/migrations/2021_03_19_104106_create_companies_table.php
+++ database/migrations/2021_03_19_104106_create_companies_table.php
@@ -14,17 +14,35 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-			$table->string('company_name');
-			$table->string('address');
-			$table->string('second_address');
-			$table->string('latitude');
-			$table->string('longitude');
-			$table->string('number_emp');
-			$table->string('mobile');
+
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('mobile');
             $table->string('email');
             $table->string('password');
+            $table->string('company_name');
+            $table->bigInteger('gst_number');
+            $table->longText('address');
+            $table->string('location');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('pincode');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('total_experience');
+            $table->string('years_of_establishment');
+            $table->enum('status',['Pending','Active']);
+            $table->boolean('is_online')->default(0);
+            $table->boolean('is_branch_available')->default(0);
+            $table->string('branch_name')->nullable();
+            $table->string('branch_place')->nullable();
+            $table->enum('user_target',['online','offline'])->default('online');
+            $table->string('no_of_employees')->nullable();
+            $table->string('nearby_famous_landmark')->nullable();
+            $table->string('targeted_age_group')->nullable();
+            $table->boolean('is_deleted')->default(0);
+            $table->dateTime('expired_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
