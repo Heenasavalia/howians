b430f1d43cd13804c9ac5bbd43fe4d020f1a780b
unknown <nimu.adbiz@gmail.com>
2021-04-16 23:57:34 +0530

    job requirements create

---
+++ database/migrations/2021_04_16_171536_create_job_requirements_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobRequirementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_requirements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('email');
+            $table->boolean('is_display_email')->default(1);
+            $table->text('title');
+            $table->text('designation');
+            $table->string('number_of_vacancy')->default(1)->nullable();
+            $table->string('minimum_salary')->nullable();
+            $table->string('maximum_salary')->nullable();
+            $table->text('other_salary_detail')->nullable();
+            $table->string('start_time')->nullable();
+            $table->string('end_time')->nullable();
+            $table->string('work_experience')->nullable();
+            $table->enum('job_type',['part_time','fulltime','freelacer'])->nullable();
+            $table->enum('work_type',['online','offline'])->nullable();
+            $table->longText('discription')->nullable();
+            $table->longText('address')->nullable();
+            $table->string('location')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->string('pincode')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_requirements');
+    }
+}
