8ea5322226446536775dd575813679f9c66fa235
heenasavaliya2909 <heenasavaliya2704@gmail.com>
2021-04-15 22:43:27 -0700

    add field migration for company

---
+++ database/migrations/2021_04_16_051345_add_field_company_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->string('company_name');
+            $table->bigInteger('gst_number')->nullable();
+            $table->longText('address');
+            $table->string('location')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->string('pincode')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('total_experience')->nullable();
+            $table->string('years_of_establishment')->nullable();
+            $table->enum('status',['Pending','Active']);
+            $table->boolean('is_online')->default(0);
+            $table->boolean('is_branch_available')->default(0);
+            $table->string('branch_name')->nullable();
+            $table->string('branch_place')->nullable();
+            $table->enum('user_target',['online','offline'])->default('online');
+            $table->string('no_of_employees')->nullable();
+            $table->string('nearby_famous_landmark')->nullable();
+            $table->string('targeted_age_group')->nullable();
+            $table->boolean('is_deleted')->default(0);
+            $table->dateTime('expired_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
