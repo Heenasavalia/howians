f5d7e9d4bfb26917ecd7aec29552cc179f8877ad
heenasavalia <heenasavaliya2704@gmail.com>
2021-05-09 19:29:26 +0530

    working on add job add skill field

--- app/JobRequirement.php
+++ app/JobRequirement.php
@@ -8,11 +8,12 @@ class JobRequirement extends Model
 {
     protected $table  = "job_requirements";
     protected $fillable = [
-        'company_id','job_category','education','email','is_display_email','title','designation','number_of_vacancy',
+        'company_id','job_category','education','email','is_display_email','title','designation',
+        'number_of_vacancy',
         'minimum_salary','maximum_salary','other_salary_detail','gender',
         'start_time','end_time','work_experience','work_experiance_type',
         'job_type','work_type','discription','address','location','latitude','longitude',
-        'pincode','city','state','country',
+        'pincode','city','state','country','require_skill'
     ];
 
     public function company()
