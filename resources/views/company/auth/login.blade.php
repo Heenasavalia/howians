307c38b8c3a49c445fdb04d8095663f9f2b79bba
Pravinadbiz <pravin.adbiz@gmail.com>
2021-06-16 23:42:54 +0530

    login and register page redesign with responsive

--- resources/views/company/auth/login.blade.php
+++ resources/views/company/auth/login.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-11 col-md-6 col-xl-4 text-center p-0 mt-3 mb-2">
+        <div class="col-11 col-md-6 col-xl-3 text-center p-0 mt-3 mb-2">
             <div class="card px-0 pt-4 pb-0 mt-3 mb-3 form-box">
-                <h2 id="heading">Sign In</h2>
+                <h2 id="heading">Login</h2>
                 <form class="form-horizontal commen-form" role="form" method="POST" action="{{ url('/company/login') }}">
                     {{ csrf_field() }}
                     <div class="form-group">
@@ -22,16 +22,17 @@
                         <span class="help-block">{{ $errors->first('password') }}</span>
                         @endif
                     </div>
-                    <div class="form-group">
-                        <div class="checkbox">
+                    <div class="form-group row">
+                        <div class="col-md-6 text-left"><div class="checkbox">
                             <label><input type="checkbox" name="remember"> <span>Remember me</span></label>
-                        </div>
+                        </div></div>
+                        <div class="col-md-6 text-right"><a class="btn btn-link" href="{{ url('/company/password/reset') }}"> Forgot Password?</a></div>
                     </div>
                     <div class="form-group text-center">
                         <button type="submit" class="action-button">Sign in</button>
-                        <a class="btn btn-link" href="{{ url('/company/password/reset') }}"> Forgot Password?</a>
+                        
                         <div class="new-account mt-3">
-                            <p>Don't have an account? <a href="{{ url('/company/register') }}">Sign up</a></p>
+                            <p><a href="{{ url('/company/register') }}">Create New Account</a></p>
                         </div>
                     </div>
                 </form>
