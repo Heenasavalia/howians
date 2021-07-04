5a34e9f89fd0571fb8e650783c3f5e81ea1b39b6
Pravinadbiz <pravin.adbiz@gmail.com>
2021-07-04 18:27:45 +0530

    css changes

--- public/front_end/js/app.js
+++ public/front_end/js/app.js
@@ -30,24 +30,6 @@ $(document).ready(function() {
         else sticky.removeClass('header-shrink');
     });
 
-    $('.reviews_box').owlCarousel({
-        loop: true,
-        margin: 0,
-        center: true,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            992: {
-                items: 3
-            }
-        }
-    });
-
     // Counter
     function isCounterElementVisible($elementToBeChecked) {
         var TopView = $(window).scrollTop();
